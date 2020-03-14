6878ac81b25a5e00ff550c8e77d6e48930935dda
吴聿轩 <929003281@qq.com>
2020-03-14 14:21:30 +0800

    upgrade useHighlight

--- src/useHighlight.ts
+++ src/useHighlight.ts
@@ -1,10 +1,9 @@
-import { useState, useEffect } from 'react';
-import { Chunk, find } from 'react-highlight';
+import { useMemo } from 'react';
+import { find } from 'react-highlight';
 
 export default function useHighlight(text: string, searchWords: string[]) {
-  const [chunks, setChunks] = useState<Chunk[]>([{ text, highlight: false }]);
-  useEffect(() => {
-    setChunks(find(text, searchWords));
-  }, [text, searchWords]);
-  return chunks;
+  return useMemo(() => find(text, searchWords), [
+    text,
+    JSON.stringify(searchWords)
+  ]);
 }
